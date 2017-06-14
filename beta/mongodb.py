9e81399eacbc9efd2bd32239ef9e5c206e32c2e2
tamir <TamirAmid@gmail.com>
2017-06-14 15:16:16 +0000

    Add files via upload

--- beta/mongodb.py
+++ beta/mongodb.py
@@ -1,46 +1,44 @@
 import sys
 import pymongo
 
+
 SEED_DATA = [
     {
         'id': 1,
         'name': 'try',
         'measure': 5,
-    },
-    {
-        'id': 2,
-        'name': 'try2',
-        'measure': 10,
-    },
-    {
-        'id': 3,
-        'name': 'try3',
-        'measure': 15,
     }
     ]
-    uri = 'mongodb://net_photo:net.photo456@ds111771.mlab.com:11771/net_photographs'
+uri = 'mongodb://net_photo:net.photo456@ds111771.mlab.com:11771/net_photographs'
 
-    def main(args):
+def main():
 
     client = pymongo.MongoClient(uri)
 
-    db = client.get_default_database()
+    db = client['emotions']
 
-    photos = db['photos']
+    emotions = db.emotions
 
-    photos.insert_many(SEED_DATA)
+    #emotions.insert_many(SEED_DATA)
 
-    query = {'id': 1}
+    #query = {'id': 1}
 
-    photos.update(query, {'$set': {'name': 'success'}})
+    
+    #emotions.update({'id' : 1}, {'$set': {'name': 'success'}})
 
-    cursor = photos.find({'name': {'success'}}).sort('id', 1)
-
+    cursor = emotions.find({'id': {1}})
     for doc in cursor:
-        print ('In the %s, %s by %s topped the charts for %d straight weeks.' %
-               (doc['decade'], doc['song'], doc['artist'], doc['weeksAtOne']))
+        print(doc)
+    
+    #for doc in cursor:
+        #print ('there was %s in id: %s and measure is %s ' %
+               #(doc['name'], doc['id'], doc['measure']))
 
-    #db.drop_collection('songs')
-    #db.drop_collection('songs')
-    if __name__ == '__main__':
-    main(sys.argv[1:])
+    #db.drop_collection('emotions')
+    client.close()
+
+
+main()
+
+
+
