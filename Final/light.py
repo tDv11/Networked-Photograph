1cc410fb01bf4fd56173b7a2db76c7f2de94447e
tamir <TamirAmid@gmail.com>
2017-06-15 21:13:06 +0300

    Create light.py

--- Final/light.py
+++ Final/light.py
@@ -5,43 +5,44 @@ import urllib.request
 import cv2
 from phue import Bridge
 
-# photo attributes
-jump = 19  # CR: consts should be UPPER_CASE
-maxpower = 254  # CR: separate words with underscore `maxpower` => `max_power`
-minpower = 60
-lightid = 1
-# CR: wrap with a function
-# read bridge ip from web
-with urllib.request.urlopen(r"https://www.meethue.com/api/nupnp") as url:
-    data = json.loads(url.read().decode())
-    ip = data[0]['internalipaddress']
-    id = data[0]['id']  # CR: overrides builtin `id` function + unused variable
+
 
 def change_light( prev_faces, curr, prev_power):
+    # photo attributes
+    JUMP = 19  # CR: consts should be UPPER_CASE
+    max_power = 254  # CR: separate words with underscore `maxpower` => `max_power`
+    min_power = 60
+    light_id = 1
+    
+    # read bridge ip from web
+    with urllib.request.urlopen(r"https://www.meethue.com/api/nupnp") as url:
+        data = json.loads(url.read().decode())
+        ip = data[0]['internalipaddress']
+    
     # philips hue light connection
     b = Bridge(ip)
     # b.connect()
     power = 0
     diff = 0
     
-    if( prev_faces > curr ):  # CR: not need for parentheses to surround `if` statements
+    if prev_faces > curr :
         diff = prev_faces - curr
-        power = jump * (prev_power - diff)
+        power = JUMP * (prev_power - diff)
 
-    if ( prev_faces < curr ):
+    if  prev_faces < curr :
         diff = curr - prev_faces
-        power = jump * (curr + diff)
-        
-    if( power > maxpower ):
+        power = JUMP * (curr + diff)
+    
+    # calibrate if overflow    
+    if power > maxpower :
         power = maxpower
-    if( power < minpower ):
+    if power < minpower :
         power = minpower
+    
+    # change light power
     b.set_light(lightid, 'bri', power)
-    
-    # CR: remove excess empty lines
-    
-    
-    
-    
-    prev_power = power  # CR: this doesn't effect the value outside of the function
+
+    prev_power = power
     prev_faces = curr
+    
+    return ( prev_faces, prev_power )
