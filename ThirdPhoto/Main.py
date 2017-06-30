23a17a60b7301e03b8459cc3285af9c56e7fc9d6
tamir <TamirAmid@gmail.com>
2017-06-30 13:05:01 +0000

    Add files via upload

--- ThirdPhoto/Main.py
+++ ThirdPhoto/Main.py
@@ -25,7 +25,7 @@ def main():
     cap.set(4, 240)
 
     pygame.mixer.init()
-    pygame.mixer.music.load("3 not so miscellany.")
+    pygame.mixer.music.load("3 not so miscellany.mp3")
 
     face_time = 0
     # init face_time from DB
