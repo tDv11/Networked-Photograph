d8ef7940e8496bbe54bb37000c2f45d510581949
tamir <TamirAmid@gmail.com>
2017-06-30 14:48:44 +0000

    Add files via upload

---
+++ twelvePhoto/light.py
@@ -0,0 +1,58 @@
+
+import sys
+import urllib.request, json
+import phue
+
+import cv2
+from phue import Bridge
+
+# read bridge ip from web
+with urllib.request.urlopen(r"https://www.meethue.com/api/nupnp") as url:
+    data = json.loads(url.read().decode())
+    ip = data[0]['internalipaddress']
+
+def change_light( prev_faces, curr, prev_power):
+      
+    # photo attributes
+    JUMP = 51
+    max_power = 254  
+    min_power = 51
+    light_id = 12
+    
+    
+    # philips hue light connection
+    while True:
+        try:
+          b = Bridge(ip)
+          break  
+        except phue.PhueRegistrationException :
+            print('please press connect button on bridge')
+    
+    power = min_power
+    diff = 0
+    
+    if prev_faces > curr :
+        diff = prev_faces - curr
+        
+
+    if  prev_faces < curr :
+        diff = curr - prev_faces
+
+    
+    
+    power += ( JUMP *  (curr) )
+    
+    
+    # calibrate if overflow
+    if power > max_power :
+        power = max_power
+    if power < min_power :
+        power = min_power
+
+    # change light power
+    b.set_light(light_id, 'bri', int(power))
+
+    prev_power = power
+    prev_faces = curr
+    
+    return ( prev_faces, prev_power )
