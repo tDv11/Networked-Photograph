878ca07e2d8c2f7636c7142dbfec0abf9f96225a
tamir <TamirAmid@gmail.com>
2017-07-06 09:26:00 +0000

    Add files via upload

--- rotshield/MainFromGallery.py
+++ rotshield/MainFromGallery.py
@@ -22,7 +22,7 @@ while True:
         tn = telnetlib.Telnet('10.0.0.100','23')
         break
     except OSError as e:
-        print(e)      
+        print(e)         
 
 #print(tn.read_all())
 
@@ -37,66 +37,72 @@ while True:
     print('call to DB as started ' + str(datetime.datetime.now()))
     cursor_one = simulation.find({'photoID':1})
     for doc in cursor_one:
-        face_one = doc['appEntranceCounter']
+        face_one = doc['currentViewers']
 
     cursor_two = simulation.find({'photoID':2})
     for doc in cursor_two:
-        face_two = doc['appEntranceCounter']
+        face_two = doc['currentViewers']
 
     cursor_three = simulation.find({'photoID':3})
     for doc in cursor_three:
-        face_three = doc['appEntranceCounter']
+        face_three = doc['currentViewers']
 
     cursor_four = simulation.find({'photoID':4})
     for doc in cursor_four:
-        face_four = doc['appEntranceCounter']
+        face_four = doc['currentViewers']
 
     cursor_five = simulation.find({'photoID':5})
     for doc in cursor_five:
-        face_five = doc['appEntranceCounter']
+        face_five = doc['currentViewers']
 
     cursor_six = simulation.find({'photoID':6})
     for doc in cursor_six:
-        face_six = doc['appEntranceCounter']
+        face_six = doc['currentViewers']
 
     cursor_seven = simulation.find({'photoID':7})
     for doc in cursor_seven:
-        face_seven = doc['appEntranceCounter']
+        face_seven = doc['currentViewers']
 
     cursor_eight = simulation.find({'photoID':8})
     for doc in cursor_eight:
-        face_eight = doc['appEntranceCounter']
+        face_eight = doc['currentViewers']
 
     cursor_nine = simulation.find({'photoID':9})
     for doc in cursor_nine:
-        face_nine = doc['appEntranceCounter']
+        face_nine = doc['currentViewers']
 
     cursor_ten = simulation.find({'photoID':10})
     for doc in cursor_ten:
-        face_ten = doc['appEntranceCounter']
+        face_ten = doc['currentViewers']
 
     cursor_eleven = simulation.find({'photoID':11})
     for doc in cursor_eleven:
-        face_eleven = doc['appEntranceCounter']
+        face_eleven = doc['currentViewers']
 
     cursor_twelve = simulation.find({'photoID':12})
     for doc in cursor_twelve:
-        face_twelve = doc['appEntranceCounter']
+        face_twelve = doc['currentViewers']
 
     cursor_thirteen = simulation.find({'photoID':13})
     for doc in cursor_thirteen:
-        face_thirteen = doc['appEntranceCounter']
-    
+        face_thirteen = doc['currentViewers']
     print('call to db as ended ' +str(datetime.datetime.now()))
     A = face_one + face_two + face_three + face_four + face_five
     B = face_six + face_seven + face_eight + face_nine + face_ten + face_eleven + face_twelve + face_thirteen
     if( A + B == 0  ) or ( last_A+last_B == A+B ):
+        lights_81=random.randint(0, 99)
+        lights_84=int(100-lights_81)
+        '''
         tn.write(('#output,81,1,65\x0d\x0a').encode('ascii'))
         tn.write(('#output,84,1,35\x0d\x0a').encode('ascii'))
+    '''
+        tn.write(('#output,81,1,'+str(lights_81)+'65\x0d\x0a').encode('ascii'))
+        tn.write(('#output,84,1,'+str(lights_84)+'\x0d\x0a').encode('ascii'))
+        print ('lights 81 = '+str(lights_81))
         print('there was no move in the gallery, light is set to 65/35')
         time.sleep(random.randint(20, 35))
-        tn.write(('#output,84,1,65\x0d\x0a').encode('ascii'))
-        tn.write(('#output,81,1,35\x0d\x0a').encode('ascii'))
+        tn.write(('#output,84,1,'+str(lights_81)+'\x0d\x0a').encode('ascii'))
+        tn.write(('#output,81,1,'+str(lights_84)+'\x0d\x0a').encode('ascii'))
         print('north and south lights power have been switch')
         time.sleep(random.randint(20, 30))
     else:
