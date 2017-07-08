ea8526c3c6f8aa5ba658efa2866b62e06955f48e
tamir <TamirAmid@gmail.com>
2017-07-08 11:34:13 +0000

    Add files via upload

---
+++ doomsDayLight.py
@@ -0,0 +1,32 @@
+import urllib.request, json
+import sys
+import phue
+from phue import Bridge
+import random
+import time
+
+# read bridge ip from web
+with urllib.request.urlopen(r"https://www.meethue.com/api/nupnp") as url:
+    data = json.loads(url.read().decode())
+    ip = data[0]['internalipaddress']
+
+# philips hue light connection
+while True:
+    try:
+        b = Bridge(ip)
+        break  
+    except phue.PhueRegistrationException :
+        print('please press connect button on bridge')
+
+while True:
+
+    my_random_light = random.randint(1, 13)
+    my_random_power = random.randint(170, 254)
+    b.set_light(my_random_light, 'bri',my_random_power )
+    print('light: ' +str(my_random_light)  + ' power: ' +str(my_random_power))
+    time.sleep(random.randint(10, 15))
+
+    my_random_power = random.randint(10, 80)
+    b.set_light(my_random_light, 'bri',my_random_power )
+    print(' next power: ' +str(my_random_power))
+    time.sleep(random.randint(5, 10))
